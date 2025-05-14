c112544a701aaa757dd8a837fe820742aa3ab996
EdoCirone <edoardocirone@gmail.com>
2025-05-14 08:32:15 +0200

    Initial commit

---
+++ Assets/Scripts/Character_Scripts/Archer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEditor;
+using UnityEngine;
+
+public class Archer : Character
+{
+
+    public int damage = 7;
+    public int numerofreccie = 2;
+
+    public override void Attack()
+    {
+        for (int i = 0; i < numerofreccie; i++)
+        {
+
+            Debug.Log($" ho lanciato la freccia numero {i} e ho fatto {damage} danni");
+
+        }
+    }
+
+    public override void TakeDamage()
+    {
+
+    }
+
+    public Archer(string nome, float hp,): base( nome, hp)
+    {
+        nome = "Archer";
+        hp = 30;
+    }
+
+}
+
---
+++ Assets/Scripts/Proiettili/Proiettile.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Proiettile
+{
+
+    float spd = 0f;
+
+
+    {
+       public abstract Lancia(_spd)
+        {
+
+        }
+    }
+
+}
